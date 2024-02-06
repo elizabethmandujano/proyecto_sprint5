96a9433544eaf66e4d4478bd6c65439eeea05c99
ElizabethMandujano <elizabethmandu@gmail.com>
2024-02-06 23:55:20 +0400

    correcion codigo select_price

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ if hist_button:
     st.plotly_chart(fig_hist, use_container_width=True)
 
 #grafico de dispersión 
-disp_button= st.button('Show the prices')
+disp_box= st.checkbox('Show the prices')
 
-if disp_button:
+if disp_box:
     st.write('Creating scatter plot about the relation between prices and odometer')
     fig_scatter = px.scatter(car_data,
                             x='odometer',
@@ -32,21 +32,10 @@ if disp_button:
                             )
     st.plotly_chart(fig_scatter, use_container_width=True)
 
-#grafico con checkbox
-box_graph= st.checkbox('Show condition of the cars')
-
-if box_graph:
-    st.write('Creating a graphic for the condition of the cars')
-    fig_graph = px.histogram(car_data,
-                             x='model_year',
-                             y= 'condition',
-                             title='Condition of cars through the years')
-    st.scatter_chart(fig_graph, color='#FF0000', use_container_width=True)
-
 #slider de rango de precios
 ## se analizó previamente cual era el precio maximo 
 select_price= st.slider('Price', 1, 375000, 100000)
-filtered_data= car_data[car_data['price'] == select_price]
+filtered_data= car_data[car_data['price'] <= select_price]
 
 st.subheader(f'Cars available for {select_price} dollars')
 st.write(filtered_data)
